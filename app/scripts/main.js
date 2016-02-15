fd8a57e437466432255b560b2cee43e85a2ff0ce
Nathan Bunney <nathan@Inventaweb.net>
2016-02-15 09:04:30 -0800

    Fixed editing bugs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,21 +7,46 @@ $.fn.serializeObject = function()
             if (!o[this.name].push) {
                 o[this.name] = [o[this.name]];
             }
-            o[this.name].push(this.value || '');
+            o[this.name].push(this.value.trim() || '');
         } else {
-            o[this.name] = this.value || '';
+            o[this.name] = this.value.trim() || '';
         }
     });
     return o;
 };
 
+$.fn.isValid = function(){
+  return this[0].checkValidity()
+}
+
+if (!String.prototype.trim) {
+  (function() {
+    // Make sure we trim BOM and NBSP
+    var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+    String.prototype.trim = function() {
+      return this.replace(rtrim, '');
+    };
+  })();
+}
+
+function olength(o){
+  var count = 0;
+  var i;
+
+  for (i in o) {
+    if (a.hasOwnProperty(i)) {
+      count++;
+    }
+  }
+  return count;
+}
 
 (function($) {
 	$(function() {
 	  ht = $(window).height() - 28;
 
   	$('.cancelButton').click(function(){
- 			var rel = $(this).parent().parent().attr('rel').split('|');
+ 			var rel = $(this).parent().parent().attr('data-rel').split('|');
 			var id = rel[1];
 			var pageID = rel[0];
 	  	$.post(SITE_PATH+'editSave', {divID: id, pageID: pageID, edit: -1}, function(){
@@ -30,7 +55,7 @@ $.fn.serializeObject = function()
     });
 
   	$('.approveButton').click(function(){
- 			var rel = $(this).parent().parent().attr('rel').split('|');
+ 			var rel = $(this).parent().parent().attr('data-rel').split('|');
 			var id = rel[1];
 			var pageID = rel[0];
 	  	$.post(SITE_PATH+'editSave', {divID: id, pageID: pageID, edit: 2}, function(){
@@ -53,7 +78,7 @@ $.fn.serializeObject = function()
   		  $('.cancelButton').hide();
   		  $('.approveButton').hide();
 
-  			var rel = $(this).parent().parent().attr('rel').split('|');
+  			var rel = $(this).parent().parent().attr('data-rel').split('|');
   			var id = $(this).parent().parent().attr('id');
         console.log(rel);
   			$('#saveID').val(rel[1]);
@@ -62,7 +87,7 @@ $.fn.serializeObject = function()
   			var wt = $('#'+id).width()+40;
 
         tinymce.init({
-          content_css : "/css/main.css,/css/site.css,/css/editor.css",
+          content_css : "/css/main.css,/css/edit.css",
           selector: '#editor_'+rel[1],theme: "modern",width: '100%',height: 300,
           plugins: [
                "advlist autolink link image lists charmap print preview hr anchor pagebreak",
@@ -71,7 +96,7 @@ $.fn.serializeObject = function()
                "code"
          ],
          toolbar1: "undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | styleselect",
-         toolbar2: "| responsivefilemanager | link unlink anchor | image media | forecolor backcolor  | print preview code ",
+         toolbar2: "| responsivefilemanager | link unlink anchor | image media | forecolor backcolor  | print preview code | fontselect fontsizeselect",
          image_advtab: true ,
          external_filemanager_path:"/filemanager/",
          filemanager_title:"Responsive Filemanager" ,
